395e97f61d7b758b1b8a60158fd57558c702ffff
Md Afsar <afsarsayyyed@gmail.com>
2025-11-02 18:09:50 +0530

    set-up database - convex

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+import { AuthConfig } from "convex/server";
+
+export default {
+  providers: [
+    {
+    
+      domain: "https://distinct-reindeer-70.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ]
+} satisfies AuthConfig;
